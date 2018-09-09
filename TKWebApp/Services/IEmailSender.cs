fb6081e6216df4db27cf50c0a27d5aaa5e7508b2
taylorkelseydude <taylorkelseydude@gmail.com>
2018-09-08 19:12:16 -0500

    initial project creation

---
+++ TKWebApp/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TKWebApp.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
